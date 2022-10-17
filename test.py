7828b9bcebc4c805e5d3e913002240f18d3540f2
Kacper Ciążyński <115944998+gimilzagar@users.noreply.github.com>
2022-10-17 08:39:29 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+import numpy as np
+M = int(input("Podaj ilość strumieni ruchu M: "))
+a = []
+t = []
+for i in range(0, M):
+    a.append(int(input("podaj ruch oferowany - a"+format(i)+": ")))
+    t.append(int(input("podaj ruch oferowany - t"+format(i)+": ")))
+V = int(input("Podaj pojemność wiązki V: "))
+n = int(input("Podaj lcizbę kanałów n: "))
+xn = [1]
+print(a, ";", t)
